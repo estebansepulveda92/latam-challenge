4ecc6124da0b031f3f6fcf77a32ff58e5de9b3c1
estebansepulveda92 <sepulveda.estebaneduardo@gmail.com>
2024-09-26 18:12:29 -0300

    feat develop: Se agrega el archivo q1_memory y se realiza la prueba en el nb challenge, donde se muestra nuestra ejecucion

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,5 +1,111 @@
 from typing import List, Tuple
 from datetime import datetime
+import json
+
+
+def _get_top_10_dates(file_path: str) -> List[str]:
+    """
+    Las top 10 fechas donde hay más tweets. .
+
+    Params
+    -------
+    file_path (str): path de json.
+
+    Returns
+    -------
+    Type: string
+    Detail: return our date format 'YYYY-MM-DD'.
+    """
+    tweets_by_date= {}
+
+    try:
+        with open(file_path, 'r') as f:
+            for line in f:
+                tweet= json.loads(line)
+                day= tweet['date'].split("T")[0]
+                tweets_by_date[day]= tweets_by_date.get(day, 0) + 1
+
+        # TOP 10
+        return [
+            day for day, _ in sorted(
+                tweets_by_date.items(), key=lambda x: x[1], reverse=True
+            )[:10]
+        ]
+
+    except FileNotFoundError:
+        print(f"Error: Archivo '{file_path}' no encontrado.")
+        return []
+
+    except json.JSONDecodeError:
+        print(f"Error: Formato de json invalido '{file_path}'.")
+        return []
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+
+def _get_most_common_username(file_path: str, day: str) -> Tuple[datetime.date, str]:
+    """
+    Username with the most tweets for our date
+
+    Args
+    ------
+    file_path (str): path JSON.
+    day (str): date in format "YYYY-MM-DD".
+
+    Returns
+    ------
+    Tupla:
+        - date (datetime.date): the date with the most tweets.
+        - username (str): the most frequent username for that date.
+    """
+    usernames = {}
+    
+    try:
+        with open(file_path, 'r') as f:
+            for line in f:
+                tweet= json.loads(line)
+
+                if tweet['date'].split("T")[0]== day:
+                    usernames[tweet['user']['username']]= usernames.get(
+                        tweet['user']['username'], 0
+                    ) +1
+        
+        day = datetime.strptime(day, "%Y-%m-%d").date()
+
+        return (day, max(usernames, key=usernames.get))
+
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        return (None, None)
+
+    except json.JSONDecodeError:
+        print(f"Error: Invalid JSON format in file '{file_path}'.")
+        return (None, None)
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return (None, None)
+
 
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    """
+    Return our top 10 dates with the most tweets and the username who tweet the most.
+
+    Args
+    -----
+    file_path (str): JSON path file.
+
+    Returns
+    -----
+    Tuple:
+        - date (datetime.date): date with the most tweets.
+        - username (str): user who tweeted the most for that date.
+    """
+
+    days= _get_top_10_dates(file_path)
+
+    return [
+        _get_most_common_username(file_path, day)
+        for day in days
+    ]
